8a318d44eaa7adad5da628280814f2199e3449f8
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2021-02-23 13:22:34 +0600

    hotfix

--- cache/admins/common/src/commonMain/kotlin/dev/inmo/tgbotapi/libraries/cache/admins/MessageSenderVerification.kt
+++ cache/admins/common/src/commonMain/kotlin/dev/inmo/tgbotapi/libraries/cache/admins/MessageSenderVerification.kt
@@ -4,8 +4,6 @@ import dev.inmo.tgbotapi.types.ChatId
 import dev.inmo.tgbotapi.types.UserId
 import dev.inmo.tgbotapi.types.message.abstracts.*
 
-suspend fun AdminsCacheAPI.userIsAdmin(chatId: ChatId, userId: UserId) = this.isAdmin()
-
 suspend fun AdminsCacheAPI.verifyMessageFromAdmin(message: ContentMessage<*>) = when (message) {
     is CommonGroupContentMessage<*> -> isAdmin(message.chat.id, message.user.id)
     is AnonymousGroupContentMessage<*> -> true
