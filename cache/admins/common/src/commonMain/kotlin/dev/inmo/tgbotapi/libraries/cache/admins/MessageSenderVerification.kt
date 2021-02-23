71f5bfc9f9ccd0f13b252b70992b393ffd113d08
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2021-02-23 13:18:22 +0600

    add several verifications extensions

---
+++ cache/admins/common/src/commonMain/kotlin/dev/inmo/tgbotapi/libraries/cache/admins/MessageSenderVerification.kt
@@ -0,0 +1,22 @@
+package dev.inmo.tgbotapi.libraries.cache.admins
+
+import dev.inmo.tgbotapi.types.ChatId
+import dev.inmo.tgbotapi.types.UserId
+import dev.inmo.tgbotapi.types.message.abstracts.*
+
+suspend fun AdminsCacheAPI.userIsAdmin(chatId: ChatId, userId: UserId) = this.isAdmin()
+
+suspend fun AdminsCacheAPI.verifyMessageFromAdmin(message: ContentMessage<*>) = when (message) {
+    is CommonGroupContentMessage<*> -> isAdmin(message.user.id, message.chat.id)
+    is AnonymousGroupContentMessage<*> -> true
+    else -> false
+}
+
+suspend fun <R> ContentMessage<*>.doAfterVerification(adminsCacheAPI: AdminsCacheAPI, block: suspend () -> R): R? {
+    val verified = adminsCacheAPI.verifyMessageFromAdmin(this)
+    return if (verified) {
+        block()
+    } else {
+        null
+    }
+}
