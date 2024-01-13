4057d5167fdf9f50a508030327a15f01bfedd704
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2024-01-13 14:37:21 +0600

    improvements in MessagesResender

--- resender/src/commonMain/kotlin/MessageMetaInfo.kt
+++ resender/src/commonMain/kotlin/MessageMetaInfo.kt
@@ -3,8 +3,10 @@ package dev.inmo.tgbotapi.libraries.resender
 import dev.inmo.tgbotapi.types.FullChatIdentifierSerializer
 import dev.inmo.tgbotapi.types.IdChatIdentifier
 import dev.inmo.tgbotapi.types.MessageId
+import dev.inmo.tgbotapi.types.message.abstracts.ContentMessage
 import dev.inmo.tgbotapi.types.message.abstracts.Message
 import dev.inmo.tgbotapi.types.message.abstracts.PossiblyMediaGroupMessage
+import dev.inmo.tgbotapi.types.message.content.MediaGroupContent
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -13,7 +15,20 @@ data class MessageMetaInfo(
     val chatId: IdChatIdentifier,
     val messageId: MessageId,
     val group: String? = null
-)
+) {
+    val metaInfo: Message.MetaInfo
+        get() = Message.MetaInfo(chatId, messageId)
+}
+
+fun Message.asMessageMetaInfos(): List<MessageMetaInfo> {
+    return if (this is ContentMessage<*>) {
+        (content as? MediaGroupContent<*>) ?.group ?.map {
+            MessageMetaInfo(it.sourceMessage.chat.id, it.sourceMessage.messageId, it.sourceMessage.mediaGroupId)
+        }
+    } else {
+        null
+    } ?: listOf(MessageMetaInfo(chat.id, messageId, (this as? PossiblyMediaGroupMessage<*>) ?.mediaGroupId))
+}
 
 operator fun MessageMetaInfo.Companion.invoke(
     message: Message
