74329a8eb262b58cad4c061c51bcc4044d054601
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2025-03-26 20:41:46 +0600

    small improvements

--- cache/content/common/src/commonMain/kotlin/dev/inmo/tgbotapi/libraries/cache/media/common/MessageContentCache.kt
+++ cache/content/common/src/commonMain/kotlin/dev/inmo/tgbotapi/libraries/cache/media/common/MessageContentCache.kt
@@ -1,5 +1,7 @@
 package dev.inmo.tgbotapi.libraries.cache.media.common
 
+import dev.inmo.tgbotapi.types.message.abstracts.ContentMessage
+import dev.inmo.tgbotapi.types.message.content.DocumentContent
 import dev.inmo.tgbotapi.types.message.content.MediaContent
 import dev.inmo.tgbotapi.types.message.content.MessageContent
 import io.ktor.utils.io.core.Input
@@ -16,7 +18,7 @@ interface MessageContentCache<K> {
         k: K,
         filename: String,
         inputAllocator: () -> Input
-    )
+    ): DocumentContent
 
     suspend fun get(k: K): MessageContent?
     suspend fun contains(k: K): Boolean
